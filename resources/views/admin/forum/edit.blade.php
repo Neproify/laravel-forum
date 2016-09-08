2d68d3aad73ad8d76c20141c4b8076fc245b2826
Neproify <yamasaki3214@gmail.com>
2016-09-08 18:28:33 +0200

    Website width change.

--- resources/views/admin/forum/edit.blade.php
+++ resources/views/admin/forum/edit.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">
                     Edycja forum <a href="$forum->getUrl()">{{ $forum->name }}</a>
--- resources/views/forum/topic/show.blade.php
+++ resources/views/forum/topic/show.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             @if (Auth::check())
                 <div class="pull-right">
                     <a class="btn btn-primary" href="{{ url('post/new', [$topic->id]) }}" role="button">Odpowiedz</a>
